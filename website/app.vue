9b68a4581b9f8ba89e184bed89b861456477c02b
OsmanthusDev <osmanthusdev@outlook.com>
2023-01-09 23:18:53 +0800

    update

--- website/app.vue
+++ website/app.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
+<style lang="scss">
+@use "@/assets/main.scss"
+</style>
--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -1,4 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
 
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=500, initial-scale=1',
+      title: 'BAIX',
+      meta: [
+        // <meta name="description" content="My amazing site">
+        { name: 'description', content: 'BAIX.' }
+      ],
+    }
+  },
+  // vite: {
+  //   css: {
+  //     preprocessorOptions: {
+  //       scss: {
+  //         additionalData: '@use "@/assets/main.scss" as *;'
+  //       }
+  //     }
+  //   }
+  // }
 })
