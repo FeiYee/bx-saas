acb5558fbc3603547d494b00cfc331198109af47
OsmanthusDev <osmanthusdev@outlook.com>
2023-02-19 16:00:20 +0800

    update

--- tools/config.py
+++ tools/config.py
@@ -2,6 +2,6 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__)
 
-DOWNLOAD_DIR = BASE_DIR / 'pubmed' / 'download'
 
 PUBMED_FILE_SERVER = 'https://ftp.ncbi.nlm.nih.gov/pubmed/updatefiles/'
+PUBMED_DOWNLOAD_DIR = BASE_DIR / 'pubmed' / 'download'
---
+++ tools/pubmed/pubmed.py
@@ -0,0 +1,5 @@
+from config import BASE_DIR, PUBMED_DOWNLOAD_DIR, PUBMED_FILE_SERVER
+
+
+def main():
+    pass
