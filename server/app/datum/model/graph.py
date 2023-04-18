d09e697b73bb9492f8737a3595bc2da1e2e1f607
FeYee <fffeiyee@163.com>
2023-04-18 16:41:52 +0000

    update server/app/datum/model/graph.py.
    Signed-off-by: FeYee <fffeiyee@163.com>

--- server/app/datum/model/graph.py
+++ server/app/datum/model/graph.py
@@ -6,6 +6,6 @@ class Graph(BaseModel):
     __tablename__ = "graph"
 
     title = Column(String(255), comment='文章标题')
-    ent1 = Column(String(255), comment='文章标题')
-    rela = Column(String(255), comment='文章标题')
-    ent2 = Column(String(255), comment='文章标题')
+    ent1 = Column(String(255), comment='起始实体')
+    rela = Column(String(255), comment='关系')
+    ent2 = Column(String(255), comment='目标实体')
