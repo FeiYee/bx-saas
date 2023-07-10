aaefc586e662e6d9238f5c0a66c2626eccb45728
FeYee <fffeiyee@163.com>
2023-07-10 08:24:56 +0000

    update server/app/datum/service/article_service.py.
    Signed-off-by: FeYee <fffeiyee@163.com>

--- server/app/datum/service/article_service.py
+++ server/app/datum/service/article_service.py
@@ -5,6 +5,7 @@ from sqlalchemy import func
 from sqlalchemy.orm import Session
 import requests
 from app.system.model.user import User
+
 from ..model.article import Article
 from ..schema.article_schema import ArticleSchema
 
@@ -34,14 +35,32 @@ class ArticleService:
             .filter(self.model.title.like('%{title}%'.format(title=title)))\
             .limit(page_size)\
             .offset((page_number-1)*page_size)
+        print("信息合并")
+        datums = self.article_info_specification(datums)
+        print("合并成功")
         return datums
+
+    def article_info_specification(self, datums):
+        datums_list = []
+        for line in datums:
+            line = line.__dict__
+
+            other = ""
+
+            if len(str(line['microbe']).replace(" ;@;", "、")) > 1:
+                other += "Microbe：" + str(line['microbe']).replace(" ;@;", "、") + "\n"
+            if len(str(line['cell']).replace(" ;@;", "、")) > 1:
+                other += "Cell：" + str(line['cell']).replace(" ;@;", "、") + "\n"
+            if len(str(line['gene']).replace(" ;@;", "、")) > 1:
+                other += "Gene：" + str(line['gene']).replace(" ;@;", "、") + "\n"
+            line["other"] = other
+            datums_list.append(line)
+            return datums_list
+
 
     def find_by_title(self, title: str, db: Session) -> List[tuple[Article]]:
         articles = db.query(self.model).filter(self.model.title.like('%{title}%'.format(title=title))).all()
-        # if len(title) == 0 or str.isspace(title):
-        #     articles = db.query(self.model).all()
-        # else:
-        #     articles = db.query(self.model).filter(self.model.title.like('%{title}%'.format(title=title))).all()
+        print(articles)
         return articles
 
     def find_article_title(self, *, db: Session) -> List[str]:
